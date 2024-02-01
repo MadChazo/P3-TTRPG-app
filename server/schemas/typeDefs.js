7175a16dae33ff397abb270cd9d37e2d145cd74f
caldardn <caldardn@gmail.com>
2024-02-01 18:38:15 -0500

    adding typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,33 @@
+const typeDefs = `
+type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    character: [Character]
+}
+
+type Character {
+    _id: ID
+    name: String
+    description: String
+}
+
+type Campaign {
+    _id: ID
+    name: String
+    description: String
+}
+
+type Auth {
+    token: ID
+    user: User
+}
+
+type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addCampaign(name: String!, description: String!, goal: Int!): Campaign
+}
+`
+module.exports = typeDefs;
