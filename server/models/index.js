77d822f7124dfdd0cf4227c1f9e369cd63acf127
MadChazo <141524368+MadChazo@users.noreply.github.com>
2024-02-01 20:26:52 -0500

    Fix a few errors, add seeds, configure server and database

---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User.js');
+const Character = require('./Character.js');
+const Campaign = require('./Campaign.js');
+
+module.exports = { User, Character, Campaign };
---
+++ server/seeds/seed.js
@@ -0,0 +1,45 @@
+const db = require("../config/connection.js");
+const { User, Character, Campaign } = require("../models");
+const cleanDB = require("./cleanDB");
+
+const userData = require("./userData.json");
+const characterData = require("./characterData.json");
+const campaignData = require("./campaignData.json");
+
+db.once("open", async () => {
+  await cleanDB("User", "users");
+  await cleanDB("Character", "characters");
+  await cleanDB("Campaign", "campaigns");
+
+  const users = await User.insertMany(userData);
+  const characters = await Character.insertMany(characterData);
+  const campaigns = await Campaign.insertMany(campaignData);
+
+  for (newCharacter of characters) {
+    const tempUser = users[Math.floor(Math.random() * users.length)];
+    tempUser.characters.push(newCharacter);
+    newCharacter.user = tempUser;
+    await tempUser.save();
+  }
+
+  for (newUser of users) {
+    const tempCampaign =
+      campaigns[Math.floor(Math.random() * campaigns.length)];
+    tempCampaign.players.push[newUser];
+    newUser.campaigns.push[tempCampaign];
+    await tempCampaign.save();
+  }
+
+  // Sets a random player as GM of each campaign
+  for (newCampaign of campaigns) {
+    const tempGM =
+      newCampaign.players[
+        Math.floor(Math.random() * newCampaign.players.length)
+      ];
+    newCampaign.gm = tempGM;
+    await tempGM.save();
+  }
+
+  console.log("Seed complete.");
+  process.exit(0);
+});
