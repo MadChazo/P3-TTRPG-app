19b0bb75b2f1063a8787f7495c47a960ce3b955c
caldardn <caldardn@gmail.com>
2024-02-01 20:00:17 -0500

    adding components and updated App & main.jsx

---
+++ client/src/components/UI/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <footer>
+            <p>Header</p>
+        </footer>
+    )
+}
+
+export default Header
---
+++ client/src/components/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+    return (
+        <footer>
+            <p>Signup</p>
+        </footer>
+    )
+}
+
+export default Signup
