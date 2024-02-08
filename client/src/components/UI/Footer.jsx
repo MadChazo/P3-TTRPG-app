68d4c5ed93cc6cb0ded1fd5b9e8880c4072c67e3
caldardn <caldardn@gmail.com>
2024-02-08 18:36:19 -0500

    adding updated footer

--- client/src/components/UI/Footer.jsx
+++ client/src/components/UI/Footer.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
     return (
-        <footer>
-            <p>Footer</p>
-        </footer>
+        <Container className="d-flex justify-content-center p-5">
+            <Row className="justify-content-between pt-6">
+                <Col xs="auto">
+                    <div className="d-flex flex-column align-items-center">
+                        <div>Madison Chazo</div>
+                        <a href="https://github.com/MadChazo" target="_blank" rel="noopener noreferrer">
+                            <FaGithub className="cursor-pointer" size={50} />
+                        </a>
+                    </div>
+                </Col>
+                <Col xs="auto">
+                    <div className="d-flex flex-column align-items-center">
+                        <div>Na Ram Park</div>
+                        <a href="https://github.com/narampark" target="_blank" rel="noopener noreferrer">
+                            <FaGithub className="cursor-pointer" size={50} />
+                        </a>
+                    </div>
+                </Col>
+                <Col xs="auto">
+                    <div className="d-flex flex-column align-items-center">
+                        <div>David Caldarone</div>
+                        <a href="https://github.com/caldardn" target="_blank" rel="noopener noreferrer">
+                            <FaGithub className="cursor-pointer" size={50} />
+                        </a>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
