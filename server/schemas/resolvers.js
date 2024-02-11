6617bc4f01ba002a10b3470052a6952bb8acd38b
GodSlappedMe <narampark92@gmail.com>
2024-02-11 15:16:52 -0500

    character and user, campaign not working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,84 +1,33 @@
 const { User, Character, Campaign } = require("../models");
-const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find().populate("characters").populate("campaigns");
+      return await User.find({}).populate("characters");
     },
-    user: async (parent, { username }) => {
-      return User.findOne({ username })
-        .populate("characters")
-        .populate("campaigns");
+    user: async (parent, { id }) => {
+      return await User.findById(id).populate("characters");
     },
     characters: async () => {
-      return Character.find().populate("user").populate("campaigns");
+      return await Character.find({}).populate("campaigns");
     },
     campaigns: async () => {
       return Campaign.find().populate("players").populate("gm");
     },
   },
-
-  Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-      if (!user) {
-        throw new AuthenticationError('Authenticartion failed');
+  Character: {
+    campaigns: async (parent) => {
+      return await Campaign.find({ _id: { $in: parent.campaigns} });
+    }
+  },
+  Campaign: {
+    gm: async (parent) => {
+      if (parent.players.length > 0) {
+        const randomIndex = Math.floor(Math.random() * parent.players.length);
+        const randomPlayerId = parent.players[randomIndex];
+        const gameMaster = await User.findById(randomPlayerId);
+        return gameMaster;
       }
-
-      const correctPw = await user.isCorrectPassword(password);
-      if (!correctPw) {
-        throw new AuthenticationError('Authentication failed');
-      }
-
-      const token = signToken(user);
-      return { token, user };
-    },
-    addCharacter: async (parent, { characterInput }, { user }) => {
-      if (!user) {
-        throw new AuthenticationError('User not authenticated');
-      }
-      const newCharacter = await Character.create({
-        ...characterInput,
-        user: user._id,
-      });
-
-      await User.findByIdAndUpdate(user._id, {
-        $push: { characters: newCharacter._id },
-      });
-
-      return newCharacter;
-    },
-    addCampaign: async (parent, { campaignInput }, { user }) => {
-      if (!user) {
-        throw new AuthenticationError('User not authenticated');
-      }
-      const newCampaign = await Campaign.create({
-        ...campaignInput,
-        gm: user._id,
-      });
-
-      await User.findByIdAndUpdate(user._id, {
-        $push: {
-          campaigns: newCampaign._id,
-        },
-      });
-
-      return newCampaign;
-    },
-    characterInCampaign: async (parent, { characterId, campaignIds }) => {
-      const updatedCharacter = await Character.findByIdAndUpdate(
-        characterId,
-        { $push: { campaigns: { $each: campaignIds } } },
-        { new: true }
-      );
-
-      return updatedCharacter;
     },
   },
 };
--- server/server.js
+++ server/server.js
@@ -1,30 +1,33 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { authMiddleware } = require("./utils/auth");
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
+
+const app = express();
 
 const startServer = async () => {
   await server.start();
-  server.applyMiddleware({ app, path: '/graphql' });
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use("/graphql", expressMiddleware(server));
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
   });
-});
 };
 
 startServer();
