a3096243a24dac8ad5514f2c9ee3424e91235b6f
MadChazo <141524368+MadChazo@users.noreply.github.com>
2024-02-13 18:28:13 -0500

    Implement character form - sort of

--- client/src/components/CharacterForm.jsx
+++ client/src/components/CharacterForm.jsx
@@ -1,5 +1,12 @@
-import { Form, Row, Col, Container } from "react-bootstrap";
-import React from "react";
+import { Form, Row, Col, Container, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+
+import { ADD_CHARACTER } from "../utils/mutations";
+import { QUERY_CHARACTERS, QUERY_ME } from "../utils/queries";
+
+import Auth from "../utils/auth";
 
 const CharForm = () => {
   const [characterInput, setCharacterInput] = useState("");
@@ -19,229 +26,237 @@ const CharForm = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
+    console.log("Input: ", characterInput);
+    console.log("Auth: ", Auth.getProfile());
     try {
       const { data } = await addCharacter({
         variables: {
-          name: characterInput.name,
-          classRole: characterInput.classRole,
-          backstory: characterInput.backstory,
-          stats: {
-            strength: characterInput.strength,
-            dexterity: characterInput.dexterity,
-            constitution: characterInput.constitution,
-            intelligence: characterInput.intelligence,
-            wisdom: characterInput.wisdom,
-            charisma: characterInput.charisma,
-          },
+          ...characterInput,
+          // user: Auth.getProfile().data._id,
         },
       });
 
-      setCharacterInput ("");
+      setCharacterInput("");
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <Form onSubmit={handleFormSubmit}>
-      <Row className="gap-4 w-full py-2 justify-content-md-center">
-        <Col md="auto">
-          <Form.Group>
-            <Form.Label
-              className="text-white"
-              style={{ display: "block", textAlign: "center" }}
-            >
-              Character Name
-            </Form.Label>
-            <Form.Control
-              style={{
-                backgroundColor: "black",
-                color: "white",
-                borderRadius: "10px",
-              }}
-              onChange={handleInputChange}
-              type="text"
-              name="name"
-              required
-            />
-          </Form.Group>
-        </Col>
-        <Col md="auto">
-          <Form.Group>
-            <Form.Label
-              className="text-white"
-              style={{ display: "block", textAlign: "center" }}
-            >
-              Class
-            </Form.Label>
-            <Form.Control
-              style={{
-                backgroundColor: "black",
-                color: "white",
-                borderRadius: "10px",
-              }}
-              onChange={handleInputChange}
-              type="text"
-              name="classRole"
-              required
-            />
-          </Form.Group>
-        </Col>
-      </Row>
+    <div>
+      {Auth.loggedIn() ? (
+        <Form onSubmit={handleFormSubmit}>
+          <Row className="gap-4 w-full py-2 justify-content-md-center">
+            <Col md="auto">
+              <Form.Group>
+                <Form.Label
+                  className="text-white"
+                  style={{ display: "block", textAlign: "center" }}
+                >
+                  Character Name
+                </Form.Label>
+                <Form.Control
+                  style={{
+                    backgroundColor: "black",
+                    color: "white",
+                    borderRadius: "10px",
+                  }}
+                  onChange={handleInputChange}
+                  type="text"
+                  name="name"
+                  required
+                />
+              </Form.Group>
+            </Col>
+            <Col md="auto">
+              <Form.Group>
+                <Form.Label
+                  className="text-white"
+                  style={{ display: "block", textAlign: "center" }}
+                >
+                  Class
+                </Form.Label>
+                <Form.Control
+                  style={{
+                    backgroundColor: "black",
+                    color: "white",
+                    borderRadius: "10px",
+                  }}
+                  onChange={handleInputChange}
+                  type="text"
+                  name="classRole"
+                  required
+                />
+              </Form.Group>
+            </Col>
+          </Row>
 
-      <Row className="justify-content-md-center">
-        <Col md={10}>
-          <Form.Group>
-            <Form.Label
-              className="text-white"
-              style={{ display: "block", textAlign: "center" }}
-            >
-              Back Story
-            </Form.Label>
-            <Form.Control
-              style={{
-                backgroundColor: "black",
-                color: "white",
-                marginBottom: "10px",
-                borderRadius: "10px",
-              }}
-              onChange={handleInputChange}
-              as="textarea"
-              rows="10"
-              name="backstory"
-            />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Container className="d-flex flex-column align-items-center min-vh-100 pb-5">
-        <Row>
-          <Col xs={12} md={6}>
-            <Form.Group>
-              <Form.Label
-                className="text-white"
-                style={{ display: "block", textAlign: "center" }}
-              >
-                Strength
-              </Form.Label>
-              <Form.Control
-                style={{
-                  backgroundColor: "black",
-                  color: "white",
-                  marginBottom: "10px",
-                  borderRadius: "10px",
-                }}
-                onChange={handleInputChange}
-                type="number"
-                name="strength"
-                required
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label
-                className="text-white"
-                style={{ display: "block", textAlign: "center" }}
-              >
-                Dexterity
-              </Form.Label>
-              <Form.Control
-                style={{
-                  backgroundColor: "black",
-                  color: "white",
-                  marginBottom: "10px",
-                  borderRadius: "10px",
-                }}
-                onChange={handleInputChange}
-                type="number"
-                name="dexterity"
-                required
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label
-                className="text-white"
-                style={{ display: "block", textAlign: "center" }}
-              >
-                Constitution
-              </Form.Label>
-              <Form.Control
-                style={{
-                  backgroundColor: "black",
-                  color: "white",
-                  borderRadius: "10px",
-                }}
-                onChange={handleInputChange}
-                type="number"
-                name="constitution"
-                required
-              />
-            </Form.Group>
-          </Col>
+          <Row className="justify-content-md-center">
+            <Col md={10}>
+              <Form.Group>
+                <Form.Label
+                  className="text-white"
+                  style={{ display: "block", textAlign: "center" }}
+                >
+                  Back Story
+                </Form.Label>
+                <Form.Control
+                  style={{
+                    backgroundColor: "black",
+                    color: "white",
+                    marginBottom: "10px",
+                    borderRadius: "10px",
+                  }}
+                  onChange={handleInputChange}
+                  as="textarea"
+                  rows="10"
+                  name="backstory"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Container className="d-flex flex-column align-items-center min-vh-100 pb-5">
+            <Row>
+              <Col xs={12} md={6}>
+                <Form.Group>
+                  <Form.Label
+                    className="text-white"
+                    style={{ display: "block", textAlign: "center" }}
+                  >
+                    Strength
+                  </Form.Label>
+                  <Form.Control
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                      marginBottom: "10px",
+                      borderRadius: "10px",
+                    }}
+                    onChange={handleInputChange}
+                    type="number"
+                    name="strength"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label
+                    className="text-white"
+                    style={{ display: "block", textAlign: "center" }}
+                  >
+                    Dexterity
+                  </Form.Label>
+                  <Form.Control
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                      marginBottom: "10px",
+                      borderRadius: "10px",
+                    }}
+                    onChange={handleInputChange}
+                    type="number"
+                    name="dexterity"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label
+                    className="text-white"
+                    style={{ display: "block", textAlign: "center" }}
+                  >
+                    Constitution
+                  </Form.Label>
+                  <Form.Control
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                      borderRadius: "10px",
+                    }}
+                    onChange={handleInputChange}
+                    type="number"
+                    name="constitution"
+                    required
+                  />
+                </Form.Group>
+              </Col>
 
-          <Col xs={12} md={6}>
-            <Form.Group>
-              <Form.Label
-                className="text-white"
-                style={{ display: "block", textAlign: "center" }}
-              >
-                Intelligence
-              </Form.Label>
-              <Form.Control
-                style={{
-                  backgroundColor: "black",
-                  color: "white",
-                  marginBottom: "10px",
-                  borderRadius: "10px",
-                }}
-                onChange={handleInputChange}
-                type="number"
-                name="intelligence"
-                required
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label
-                className="text-white"
-                style={{ display: "block", textAlign: "center" }}
-              >
-                Wisdom
-              </Form.Label>
-              <Form.Control
-                style={{
-                  backgroundColor: "black",
-                  color: "white",
-                  marginBottom: "10px",
-                  borderRadius: "10px",
-                }}
-                onChange={handleInputChange}
-                type="number"
-                name="wisdom"
-                required
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label
-                className="text-white"
-                style={{ display: "block", textAlign: "center" }}
-              >
-                Charisma
-              </Form.Label>
-              <Form.Control
-                style={{
-                  backgroundColor: "black",
-                  color: "white",
-                  borderRadius: "10px",
-                }}
-                onChange={handleInputChange}
-                type="number"
-                name="charisma"
-                required
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-      </Container>
-    </Form>
+              <Col xs={12} md={6}>
+                <Form.Group>
+                  <Form.Label
+                    className="text-white"
+                    style={{ display: "block", textAlign: "center" }}
+                  >
+                    Intelligence
+                  </Form.Label>
+                  <Form.Control
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                      marginBottom: "10px",
+                      borderRadius: "10px",
+                    }}
+                    onChange={handleInputChange}
+                    type="number"
+                    name="intelligence"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label
+                    className="text-white"
+                    style={{ display: "block", textAlign: "center" }}
+                  >
+                    Wisdom
+                  </Form.Label>
+                  <Form.Control
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                      marginBottom: "10px",
+                      borderRadius: "10px",
+                    }}
+                    onChange={handleInputChange}
+                    type="number"
+                    name="wisdom"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label
+                    className="text-white"
+                    style={{ display: "block", textAlign: "center" }}
+                  >
+                    Charisma
+                  </Form.Label>
+                  <Form.Control
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                      borderRadius: "10px",
+                    }}
+                    onChange={handleInputChange}
+                    type="number"
+                    name="charisma"
+                    required
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Button
+              variant="dark"
+              type="submit"
+              style={{ boxShadow: "3px 3px 3px 3px rgba(0, 0, 0, 0.5)" }}
+            >
+              Create
+            </Button>
+          </Container>
+        </Form>
+      ) : (
+        <p>
+          You need to be logged in to create a character. Please{" "}
+          <Link to="/login">login</Link> or <Link to="/signup">signup</Link>.
+        </p>
+      )}
+    </div>
   );
 };
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,42 @@
-import { gql } from '@apollo/client'
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-mutation loginUser($username: String!, $password: String!) {
+  mutation loginUser($username: String!, $password: String!) {
     loginUser(username: $username, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+      token
+      user {
+        _id
+        username
+      }
     }
-}
+  }
 `;
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+      token
+      user {
+        _id
+        username
+      }
     }
-}`;
+  }
+`;
+
+export const ADD_CHARACTER = gql`
+  mutation addCharacter( $name: String!, $classRole: String!, $backstory: String!, $strength: Int!, $dexterity: Int!, $constitution: Int!, $intelligence: Int!, $wisdom: Int!, $charisma: Int!) {
+    addCharacter( name: $name, classRole: $classRole, backstory: $backstory, strength: $strength, dexterity: $dexterity, constitution: $constitution, intelligence: $intelligence, wisdom: $wisdom, charisma: $charisma) {
+      _id
+      name
+      classRole
+      backstory
+      strength
+      dexterity
+      constitution
+      intelligence
+      wisdom
+      charisma
+      user
+    }
+  }
+`;
