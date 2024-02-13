8078a9922b7d50737f2d32b2049b0a06943f2c91
MadChazo <141524368+MadChazo@users.noreply.github.com>
2024-02-12 20:15:59 -0500

    more server fixes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
+const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
@@ -18,6 +20,13 @@ const server = new ApolloServer({
 const startServer = async () => {
   await server.start();
   server.applyMiddleware({ app, path: "/graphql" });
+
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
